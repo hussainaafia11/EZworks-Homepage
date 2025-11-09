6b68b2f22a34f8782e357e5ee1997799474d45b5
hussainaafia11 <142110490+hussainaafia11@users.noreply.github.com>
2025-11-09 23:54:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Update base path for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/EZworks-Homepage/', // 👈 your repo name here!
+});
